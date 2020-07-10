354471bdd9baa26bde6d55c92428f909127e2cfb
mordici <51112707+mordici@users.noreply.github.com>
2020-07-10 20:58:33 +0200

    Add files via upload

---
+++ OPV/Program.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+
+Namespace My
+
+	Partial Class MyApplication
+		Public Sub New()
+			MyBase.New(AuthenticationMode.Windows)
+			Me.IsSingleInstance = False
+			Me.EnableVisualStyles = True
+			Me.SaveMySettingsOnExit = True
+			Me.ShutDownStyle = ShutdownMode.AfterMainFormCloses
+		End Sub
+		
+		Protected Overrides Sub OnCreateMainForm()
+			Me.MainForm = My.Forms.MainForm
+		End Sub
+	End Class
+End Namespace
