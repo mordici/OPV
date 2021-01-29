1e7f1ae6bdf5defd2b91af3d665111e11383c65b
mordici <51112707+mordici@users.noreply.github.com>
2021-01-29 21:37:06 +0200

    Add files via upload

---
+++ dm/java-android/app/src/main/java/mo/mrd/debitomer/MainActivity.java
@@ -0,0 +1,181 @@
+package mo.mrd.debitomer;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.graphics.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.text.*;
+import android.app.Activity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Button;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.view.View;
+import java.text.DecimalFormat;
+
+public class MainActivity extends Activity {
+	
+	private Timer _timer = new Timer();
+	
+	private double timeElapsed = 0;
+	private double s = 0;
+	
+	private LinearLayout linear1;
+	private LinearLayout linear2;
+	private TextView textview2;
+	private LinearLayout linear4;
+	private TextView textview1;
+	private Button button1;
+	private Button button3;
+	private Button button2;
+	private TextView textview4;
+	
+	private TimerTask timer;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		linear2 = (LinearLayout) findViewById(R.id.linear2);
+		textview2 = (TextView) findViewById(R.id.textview2);
+		linear4 = (LinearLayout) findViewById(R.id.linear4);
+		textview1 = (TextView) findViewById(R.id.textview1);
+		button1 = (Button) findViewById(R.id.button1);
+		button3 = (Button) findViewById(R.id.button3);
+		button2 = (Button) findViewById(R.id.button2);
+		textview4 = (TextView) findViewById(R.id.textview4);
+		
+		textview1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				
+			}
+		});
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				s = Double.parseDouble(textview1.getText().toString());
+				timer = new TimerTask() {
+					@Override
+					public void run() {
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								s++;
+								textview1.setText(String.valueOf((long)(s)));
+							}
+						});
+					}
+				};
+				_timer.scheduleAtFixedRate(timer, (int)(1000), (int)(1000));
+			}
+		});
+		
+		button3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				textview4.setText(new DecimalFormat("").format(60 / Double.parseDouble(textview1.getText().toString())).concat(" л/м"));
+				timer.cancel();
+			}
+		});
+		
+		button2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				timer.cancel();
+				textview1.setText("0");
+				textview4.setText("");
+			}
+		});
+	}
+	private void initializeLogic() {
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	private void _SetTimerText (final double _secs) {
+		s = _secs % 60;
+		textview1.setText(String.valueOf((long)(s)));
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
