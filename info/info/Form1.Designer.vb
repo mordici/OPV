e9fc00233b31aa1f8eb7a849449173978f2f1638
mordici <51112707+mordici@users.noreply.github.com>
2022-01-07 23:02:28 +0200

    Add files via upload

--- info/info/Form1.Designer.vb
+++ info/info/Form1.Designer.vb
@@ -27,9 +27,6 @@ Partial Class Form1
         Me.label4 = New System.Windows.Forms.Label
         Me.tabControl1 = New System.Windows.Forms.TabControl
         Me.label2 = New System.Windows.Forms.Label
-        Me.Label5 = New System.Windows.Forms.Label
-        Me.Label6 = New System.Windows.Forms.Label
-        Me.Label7 = New System.Windows.Forms.Label
         Me.tabPage1.SuspendLayout()
         Me.tabPag.SuspendLayout()
         Me.tabControl1.SuspendLayout()
@@ -38,20 +35,20 @@ Partial Class Form1
         'label1
         '
         Me.label1.Font = New System.Drawing.Font("Papyrus", 18.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.label1.Location = New System.Drawing.Point(138, -1)
+        Me.label1.Location = New System.Drawing.Point(155, -31)
         Me.label1.Name = "label1"
         Me.label1.Size = New System.Drawing.Size(153, 71)
         Me.label1.TabIndex = 4
-        Me.label1.Text = "mordiclSoft" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "  OPV2.3"
+        Me.label1.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "OPV3.1"
         '
         'label3
         '
         Me.label3.Location = New System.Drawing.Point(0, 3)
         Me.label3.Name = "label3"
-        Me.label3.Size = New System.Drawing.Size(272, 68)
+        Me.label3.Size = New System.Drawing.Size(272, 52)
         Me.label3.TabIndex = 0
-        Me.label3.Text = "Версия программы: 2.3" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Язык программы: многоязычный" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Версия .NET Framework: 2" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Ве" & _
-            "рсия Windows: Windows NT" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Написана на Visual Basic .NET"
+        Me.label3.Text = "Версия программы: 3.1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Язык программы: многоязычный" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Версия .NET Framework: 2" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Ве" & _
+            "рсия Windows: Windows NT"
         '
         'tabPage1
         '
@@ -59,7 +56,7 @@ Partial Class Form1
         Me.tabPage1.Location = New System.Drawing.Point(4, 22)
         Me.tabPage1.Name = "tabPage1"
         Me.tabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.tabPage1.Size = New System.Drawing.Size(272, 86)
+        Me.tabPage1.Size = New System.Drawing.Size(272, 69)
         Me.tabPage1.TabIndex = 0
         Me.tabPage1.Text = "Системная информация"
         Me.tabPage1.UseVisualStyleBackColor = True
@@ -70,7 +67,7 @@ Partial Class Form1
         Me.tabPag.Location = New System.Drawing.Point(4, 22)
         Me.tabPag.Name = "tabPag"
         Me.tabPag.Padding = New System.Windows.Forms.Padding(3)
-        Me.tabPag.Size = New System.Drawing.Size(272, 86)
+        Me.tabPag.Size = New System.Drawing.Size(272, 69)
         Me.tabPag.TabIndex = 1
         Me.tabPag.Text = "Компоненты"
         Me.tabPag.UseVisualStyleBackColor = True
@@ -88,61 +85,25 @@ Partial Class Form1
         '
         Me.tabControl1.Controls.Add(Me.tabPage1)
         Me.tabControl1.Controls.Add(Me.tabPag)
-        Me.tabControl1.Location = New System.Drawing.Point(-1, 167)
+        Me.tabControl1.Location = New System.Drawing.Point(-1, 137)
         Me.tabControl1.Name = "tabControl1"
         Me.tabControl1.SelectedIndex = 0
-        Me.tabControl1.Size = New System.Drawing.Size(280, 112)
+        Me.tabControl1.Size = New System.Drawing.Size(280, 95)
         Me.tabControl1.TabIndex = 6
         '
         'label2
         '
-        Me.label2.Location = New System.Drawing.Point(-1, 70)
+        Me.label2.Location = New System.Drawing.Point(3, 40)
         Me.label2.Name = "label2"
         Me.label2.Size = New System.Drawing.Size(292, 94)
         Me.label2.TabIndex = 5
         Me.label2.Text = resources.GetString("label2.Text")
         '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Segoe Script", 27.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(204, Byte))
-        Me.Label5.ForeColor = System.Drawing.Color.Gold
-        Me.Label5.Location = New System.Drawing.Point(-12, -1)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(57, 61)
-        Me.Label5.TabIndex = 7
-        Me.Label5.Text = "O"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Segoe Script", 27.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(204, Byte))
-        Me.Label6.ForeColor = System.Drawing.Color.White
-        Me.Label6.Location = New System.Drawing.Point(37, -1)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(56, 61)
-        Me.Label6.TabIndex = 8
-        Me.Label6.Text = "P"
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Font = New System.Drawing.Font("Segoe Script", 27.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(204, Byte))
-        Me.Label7.ForeColor = System.Drawing.Color.DarkGreen
-        Me.Label7.Location = New System.Drawing.Point(88, -1)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(54, 61)
-        Me.Label7.TabIndex = 9
-        Me.Label7.Text = "V"
-        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(277, 273)
-        Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label5)
+        Me.ClientSize = New System.Drawing.Size(278, 234)
         Me.Controls.Add(Me.label1)
         Me.Controls.Add(Me.tabControl1)
         Me.Controls.Add(Me.label2)
@@ -157,7 +118,6 @@ Partial Class Form1
         Me.tabPag.ResumeLayout(False)
         Me.tabControl1.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Private WithEvents label1 As System.Windows.Forms.Label
@@ -167,8 +127,5 @@ Partial Class Form1
     Private WithEvents label4 As System.Windows.Forms.Label
     Private WithEvents tabControl1 As System.Windows.Forms.TabControl
     Private WithEvents label2 As System.Windows.Forms.Label
-    Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents Label6 As System.Windows.Forms.Label
-    Friend WithEvents Label7 As System.Windows.Forms.Label
 
 End Class
